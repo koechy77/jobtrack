89072e93aaad4a77b6769d86245492580a762cd3
koechy77 <kingsleyonyem7@gmail.com>
2026-09-11 21:24:16 +0100

    feat: build errorhandler middleware to handle all kinds of error

---
+++ backend/src/middleware/errorHandler.js
@@ -0,0 +1,76 @@
+const AppError = require("../utils/appError");
+
+const handleJWTExpiredError = () => {
+  return new AppError("Your session has expired. Please log in again.", 401);
+};
+
+const handleJWTError = () => {
+  return new AppError("Invalid token. Please log in again.", 401);
+};
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  const value = Object.values(err.keyValue)[0];
+
+  const message = `Duplicate field value: ${value}. Please use another value.`;
+
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const message = Object.values(err.errors)
+    .map((error) => error.message)
+    .join(". ");
+
+  return new AppError(message, 400);
+};
+
+const errorhandler = (err, req, res, next) => {
+  if (process.env.NODE_ENV === "development") {
+    return res.status(err.statuscode || 500).json({
+      status: err.status || "error",
+      error: err,
+      message: err.message,
+      stack: err.stack,
+    });
+  }
+
+  if (err.name === "JsonWebTokenError") {
+    err = handleJWTError();
+  }
+
+  if (err.name === "TokenExpiredError") {
+    err = handleJWTExpiredError();
+  }
+
+  if (err.name === "CastError") {
+    err = handleCastErrorDB(err);
+  }
+
+  if (err.code === 11000) {
+    err = handleDuplicateFieldsDB(err);
+  }
+
+  if (err.name === "ValidationError") {
+    err = handleValidationErrorDB(err);
+  }
+
+  if (err.isOperational) {
+    return res.status(err.statuscode).json({
+      status: err.status,
+      message: err.message,
+    });
+  }
+
+  console.error("Technical Error:", err);
+  return res.status(500).json({
+    status: "error",
+    message: "Something went wrong!",
+  });
+};
+module.exports = errorhandler;
