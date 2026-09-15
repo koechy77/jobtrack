a756b2058f8d7e2443a096b29cc403372f791d48
koechy77 <kingsleyonyem7@gmail.com>
2026-09-15 13:39:30 +0100

    feat: build refreshtoken schema

---
+++ backend/src/schemas/refreshTokenSchema.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const refreshTokenSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    tokenHash: {
+      type: String,
+      required: true,
+      select: false,
+    },
+
+    expiresAt: {
+      type: Date,
+      required: true,
+    },
+
+    revoked: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = refreshTokenSchema;
