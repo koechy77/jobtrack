886093f7c2302d9eeb5b6ab5d8901a48cb1aca61
koechy77 <kingsleyonyem7@gmail.com>
2026-09-15 13:36:49 +0100

    feat: build refreshtoken creation functions

---
+++ backend/utils/tokenUtils.js
@@ -0,0 +1,31 @@
+const crypto = require("crypto");
+const RefreshToken = require("../models/refreshTokenModel");
+
+const generateRefreshToken = () => {
+  return crypto.randomBytes(64).toString("hex");
+};
+
+const hashToken = (token) => {
+  return crypto.createHash("sha256").update(token).digest("hex");
+};
+
+const createRefreshToken = async (userId) => {
+  const rawToken = generateRefreshToken();
+
+  const tokenHash = hashToken(rawToken);
+
+  const expiresAt = new Date(Date.now() + 30 * 24 * 60 * 60 * 1000);
+
+  const refreshToken = await RefreshToken.create({
+    user: userId,
+    tokenHash,
+    expiresAt,
+  });
+  return rawToken;
+};
+
+module.exports = {
+  generateRefreshToken,
+  hashToken,
+  createRefreshToken,
+};
